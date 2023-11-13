5f91348be0d8c4e76a80ebdba99f855d7300853c
Qalab Abbas <qalab.abbas@dfki.de>
2023-11-13 19:27:19 +0100

    Inserted comments for better understanding of function

--- extract_pcd_and_image.py
+++ extract_pcd_and_image.py
@@ -8,6 +8,17 @@ import os
 
 
 def extract_pcd_image(bag):
+
+    '''
+    Extract point cloud and image from rosbag at 20 timestamps from whole duration of rosbag.
+    The rosbag start and end time is divided into 20 equal timestamps
+    The pointcloud data from rosbag is converted to open3d Pointcloud object and then saved in .pcd format
+    The extracted images are also saved in png format
+    
+    Parameters:
+        bag (rosbag): ROS Bag file
+
+    '''
     start_time = int(bag.get_start_time())
     end_time = int(bag.get_end_time())
  
@@ -20,7 +31,7 @@ def extract_pcd_image(bag):
     for time in time_array:
         ran=False
         
-        for topic,msg,t in bag.read_messages(topics=['/ouster_cloud_os1_0/points']):    
+        for topic,msg,t in bag.read_messages(topics=['/pointcloud_topic_name']): # Give the pointcloud topic name here from which x,y,z fields of recorded data is extracted
             if int(t.to_sec())==time and not ran:
                 points=np.array(list(point_cloud2.read_points(msg, field_names = ("x", "y", "z"), skip_nans=True)))
                 pcd=o3d.geometry.PointCloud()
@@ -34,7 +45,7 @@ def extract_pcd_image(bag):
     for time in time_array:
         ran=False
         
-        for topic,msg,t in bag.read_messages(topics=['/zed2i_0/rgb_raw/image_raw_color']):    
+        for topic,msg,t in bag.read_messages(topics=['/image_topic_name']): # Give the image topic name here from which image is to be extracted     
             if int(t.to_sec())==time and not ran:            
                 cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
                 cv2.imwrite(os.path.join('images_2/', "ground_{}.png".format(time)), cv_img)
@@ -60,6 +71,6 @@ def extract_pcd_image(bag):
 
 
 
-bag=rosbag.Bag('/home/qalab/DFKI/greenland_height/krone_data/Ground_default_test/recording_2022-12-06-09-53-56_0.bag')
+bag=rosbag.Bag('rosbag.bag')  # Rosbag file path
 extract_pcd_image(bag)
 print('Extraction of pointcloud and images done')
